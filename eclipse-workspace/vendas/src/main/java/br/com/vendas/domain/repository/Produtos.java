39619efc8534c38858dbe817b863e2cbf199a65b
krsilva4 <krsilva4@hotmail.com>
2022-04-09 17:28:03 -0300

    faltou subir

---
+++ eclipse-workspace/vendas/src/main/java/br/com/vendas/domain/repository/Produtos.java
@@ -0,0 +1,8 @@
+package br.com.vendas.domain.repository;
+
+import br.com.vendas.domain.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Produtos extends JpaRepository<Produto,Long> {
+
+}
